292d7dd0753e0c7e876da1232e32adf4ddf49229
James Kim <jamesjkjk@gmail.com>
2020-06-25 16:34:23 -0700

    Added Login and Register

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-        public string Username { get; set;}
-        public string Password {get;set;}
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters")]
+        public string Password { get; set; }
     }
 }
